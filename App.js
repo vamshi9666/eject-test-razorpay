fa0ffa3cee306f27ff255a6e9073ead59e0552e6
Vamshi krishna <vamshi9666@riseup.net>
2020-06-12 13:38:45 +0530

    eject test

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Button } from 'react-native';
+import RazorpayCheckout from 'react-native-razorpay';
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+
+
+      <Button title={"Get rich"} onPress={()=> {
+        var options = {
+          description: 'Credits towards consultation',
+          image: 'https://i.imgur.com/3g7nmJC.png',
+          currency: 'INR',
+          key: 'rzp_test_1DP5mmOlF5G5ag',
+          amount: '5000',
+          name: 'foo',
+          prefill: {
+            email: 'void@razorpay.com',
+            contact: '9191919191',
+            name: 'Razorpay Software'
+          },
+          theme: {color: '#F37254'}
+        }
+        RazorpayCheckout.open(options).then((data) => {
+          // handle success
+          alert(`Success: ${data.razorpay_payment_id}`);
+        }).catch((error) => {
+          // handle failure
+          alert(`Error: ${error.code} | ${error.description}`);
+        });
+      }} />
     </View>
   );
 }
